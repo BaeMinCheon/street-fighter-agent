63f6ace2001b4aa8e4d112473e9ed3648fe25827
baemincheon <baemincheon@gmail.com>
2018-11-18 23:22:35 +0900

    EDIT server.py

--- test/server.py
+++ test/server.py
@@ -17,12 +17,13 @@ while True:
         try:
             buffer = cs.recv(BUFFER_SIZE)
             if buffer:
-                print('received data : {}'.format(buffer))
+                p1_isLeft, gap_X, gap_Y, gap_HP_for_p1, p1_canInputMove, p1_canInputAction = buffer.decode('utf8').split(".")
+                print('p1_isLeft : {} \t gap_X : {} \t gap_Y : {} \t gap_HP_for_p1 : {} \t p1_canInputMove : {} \t p1_canInputAction : {}'.format(p1_isLeft, gap_X, gap_Y, gap_HP_for_p1, p1_canInputMove, p1_canInputAction))
                 cs.send(bytearray('ACK !', 'utf-8'))
             else:
                 print('disconnected to {}'.format(addr))
                 break
-        except:
+        except socket.error:
             print('disconnected to {}'.format(addr))
             break
 
