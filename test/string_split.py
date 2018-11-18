6215364073abc9a1757801295447cb1102e1484d
baemincheon <baemincheon@gmail.com>
2018-11-18 23:22:19 +0900

    ADD string_split.py

---
+++ test/string_split.py
@@ -0,0 +1,9 @@
+
+part01, part02 = "127.0".split(".")
+print("part01 : {}".format(part01))
+print("part02 : {}".format(part02))
+
+list = []
+list = "127.0.0.1".split(".")
+for s in list:
+    print(s)
