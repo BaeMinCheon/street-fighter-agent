07b41aa55f237535b947041362098bf9c543f8da
baemincheon <baemincheon@gmail.com>
2019-05-12 13:25:53 +0900

    add textbox to input code

--- src/gui/Window.py
+++ src/gui/Window.py
@@ -3,11 +3,13 @@ import kivy.config
 import kivy.app
 import kivy.uix.button
 import kivy.uix.boxlayout
+import kivy.uix.textinput
 
 Config = kivy.config.Config
 App = kivy.app.App
 Button = kivy.uix.button.Button
 BoxLayout = kivy.uix.boxlayout.BoxLayout
+TextInput = kivy.uix.textinput.TextInput
 
 Config.set('input', 'mouse', 'mouse,disable_multitouch')
 
@@ -23,6 +25,18 @@ class RootWidget(BoxLayout):
         self.b2 = Button(text='Server Stop')
         self.b2.on_press = _manager.OnClickStop
         self.add_widget(self.b2)
+
+        self.tb1 = TextInput(text='''# write code controlling the game
+control = []
+if(_data['timer'] == 0):
+    if(self.count_frame >= 100):
+        self.count_frame = 0
+        control = [0, 0, 2]
+    else:
+        control = [0, 0, 0]
+return control
+        ''')
+        self.add_widget(self.tb1)
 
 class Window(App):
 
