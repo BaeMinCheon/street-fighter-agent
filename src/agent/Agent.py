64e7a455fad292591dad01ac98a92b2d7cb04f05
baemincheon <baemincheon@gmail.com>
2019-09-01 00:16:55 +0900

    refactor agent & network for removing DQN codes

--- src/agent/Agent.py
+++ src/agent/Agent.py
@@ -9,10 +9,15 @@ class Agent:
 
     def __init__(self, _inputSize, _outputSize, _discount):
         self.session = tf.Session()
+        self.network = Network.Network(self.session, _inputSize, _outputSize, _discount, 'main')
         self.session.run(tf.global_variables_initializer())
+        self.state = [0] * _inputSize
 
     def Input(self, _data):
-        pass
+        self.state = [_data['P1.IsLeft'], _data['Gap.X'], _data['Gap.Y'], _data['P1.CanAction']]
+        label = self.network.Decide(self.state)
+        self.network.Train([self.state], label)
 
     def Output(self):
-        return random.randrange(0, 25)
+        decision = self.network.Decide(self.state)
+        return np.argmax(decision)
--- src/agent/Network.py
+++ src/agent/Network.py
@@ -14,12 +14,12 @@ class Network:
 
     def InitNet(self, _hiddenSize, _learnRate):
         with tf.variable_scope(self.network_name):
-            self.input = tf.placeholder(tf.float32, [None, self.size_input], name='INPUT')
+            self.input = tf.placeholder(tf.float32, shape=[None, self.size_input], name='input')
 
-            weight01 = tf.get_variable(name='WEIGHT_01', shape=[self.size_input, _hiddenSize], initializer=tf.contrib.layers.xavier_initializer())
+            weight01 = tf.get_variable(name='weight_01', shape=[self.size_input, _hiddenSize], initializer=tf.contrib.layers.xavier_initializer())
             output01 = tf.nn.tanh(tf.matmul(self.input, weight01))
 
-            weight02 = tf.get_variable(name='WEIGHT_02', shape=[_hiddenSize, self.size_output], initializer=tf.contrib.layers.xavier_initializer())
+            weight02 = tf.get_variable(name='weight_02', shape=[_hiddenSize, self.size_output], initializer=tf.contrib.layers.xavier_initializer())
             output02 = tf.matmul(output01, weight02)
 
             self.output = output02
@@ -28,9 +28,9 @@ class Network:
         self.error = tf.reduce_mean(tf.square(tf.subtract(self.label, self.output)))
         self.train = tf.train.AdamOptimizer(learning_rate=_learnRate).minimize(self.error)
 
-    def Train(self, _x, _y):
-        return self.session.run([self.error, self.train], feed_dict={self.input: _x, self.label: _y})
+    def Train(self, _input, _label):
+        return self.session.run([self.error, self.train], feed_dict={self.input: _input, self.label: _label})
 
-    def Predict(self, _state):
+    def Decide(self, _state):
         reshapedState = np.reshape(_state, [1, self.size_input])
         return self.session.run(self.output, feed_dict={self.input: reshapedState})
